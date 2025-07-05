be9082d2af6c5545d22ab071e77492c25ba76eb3
UltraMurlock <89306501+UltraMurlock@users.noreply.github.com>
2025-07-05 11:17:48 +0300

    Feature: after AOE4 exit kill the process

--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
-	golang.org/x/sys v0.0.0-20190916202348-b4ddaad3f8a3 // indirect
+	golang.org/x/sys v0.33.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,11 +6,87 @@ import (
 	"time"
 
 	"github.com/yusufpapurcu/wmi"
+
+	"golang.org/x/sys/windows/svc"
+	"golang.org/x/sys/windows/svc/debug"
+	"golang.org/x/sys/windows/svc/eventlog"
 )
 
+const serviceName string = "AOE4CardinalKiller"
+const mainProcess string = "RelicCardinal.exe"
+const secondaryProcess string = "RelicCardinal.exe"
+
+type KillerService struct{}
+
+func (m *KillerService) Execute(args []string, r <-chan svc.ChangeRequest, changes chan<- svc.Status) (ssec bool, errno uint32) {
+	changes <- svc.Status{State: svc.StartPending}
+	changes <- svc.Status{State: svc.Running, Accepts: svc.AcceptStop | svc.AcceptShutdown}
+
+	var ticker time.Ticker = *time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	var isMainStarted bool = false
+mainLoop:
+	for {
+		select {
+		case <-ticker.C:
+			var processes []Win32_Process
+			var query string = fmt.Sprintf("SELECT * FROM Win32_Process WHERE Name = '%s'", mainProcess)
+
+			err := wmi.Query(query, &processes)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if !isMainStarted && len(processes) != 0 && processes[0].ThreadCount >= 60 {
+				isMainStarted = true
+				fmt.Printf("Process %s started\n", mainProcess)
+				continue
+			}
+
+			if isMainStarted && processes[0].ThreadCount < 60 {
+				isMainStarted = false
+				terminateProcessesByName(secondaryProcess)
+				fmt.Printf("Process %s stopped\n", mainProcess)
+			}
+		case c := <-r:
+			switch c.Cmd {
+			case svc.Interrogate:
+				changes <- c.CurrentStatus
+			case svc.Stop, svc.Shutdown:
+				break mainLoop
+			}
+		}
+	}
+
+	changes <- svc.Status{State: svc.StopPending}
+	return
+}
+
+func runService() error {
+	isWindowsService, err := svc.IsWindowsService()
+	if err != nil {
+		return err
+	}
+
+	if isWindowsService {
+		return svc.Run(serviceName, &KillerService{})
+	}
+
+	elog, err := eventlog.Open(serviceName)
+	if err != nil {
+		return err
+	}
+
+	elog.Info(1, "Open in console mode")
+	debug.Run(serviceName, &KillerService{})
+	return nil
+}
+
 type Win32_Process struct {
-	Name      string
-	ProcessID uint32
+	Name        string
+	ProcessID   uint32
+	ThreadCount uint32
 }
 
 func terminateProcess(pid uint32) error {
@@ -33,36 +109,7 @@ func terminateProcessesByName(name string) {
 }
 
 func main() {
-	var mainProcess string = "notepad.exe"
-	var secondaryProcess string = "notepad++.exe"
-
-	var isMainStarted bool = false
-	for {
-		var processes []Win32_Process
-		var query string = fmt.Sprintf("SELECT * FROM Win32_Process WHERE Name = '%s'", mainProcess)
-		err := wmi.Query(query, &processes)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if len(processes) != 0 {
-			isMainStarted = true
-			fmt.Printf("Process %s started\n", mainProcess)
-		}
-
-		if isMainStarted && len(processes) == 0 {
-			isMainStarted = false
-			terminateProcessesByName(secondaryProcess)
-			fmt.Printf("Process %s stopped\n", mainProcess)
-		}
-
-		var sleepDuration time.Duration
-		if isMainStarted {
-			sleepDuration = 1 * time.Second
-		} else {
-			sleepDuration = 5 * time.Second
-		}
-
-		time.Sleep(sleepDuration)
+	if err := runService(); err != nil {
+		log.Fatalf("Service error: %v", err)
 	}
 }
